07783f5c7a0a35c686a1fdff978b9385dab317b8
PandeMD <mathilipande65@gmail.com>
2023-11-29 14:14:51 +0530

    initial project add. Closes #1

---
+++ mssc-beer-service/src/main/java/ms/cloud/guru/service/beer/MsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package ms.cloud.guru.service.beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceApplication.class, args);
+	}
+
+}
